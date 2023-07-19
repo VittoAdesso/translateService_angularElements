e497ff374dbb7c8bfc41ca42b4405efbcdcbc0b1
Vittorio Adesso <adessovittorio8@gmail.com>
2023-07-19 20:46:08 +0200

    Added new folders

---
+++ src/shared/i18n/en.ts
@@ -0,0 +1,31 @@
+export default {
+  common: {
+    confirm: 'Confirm',
+    confirmPayToUniversity: 'Confirm payment to ADE',
+    confirmYourPay: 'Confirm your payment',
+    description: 'Description',
+    directDebit: 'Direct Debit',
+    directDebitPayments: 'Direct Debit Payments',
+    help: 'Help',
+    load: 'Loading...',
+    name: 'Name',
+    payConfirm: 'Confirm payment',
+    payment: 'Payment',
+    titleInfo: 'Card information',
+    university: 'ADE'
+  },
+  enrolmentTokenization: {
+    infoMessage:
+      'Fees due in the future will be charged.',
+    plan: 'Installment Plan'
+  },
+  modal: {
+    accept: 'Accept',
+    cancel: 'Cancel'
+  },
+  tokenizeCard: {
+    alert:
+      'By clicking on Confirm Payment, you agree to have this and future charges.',
+    directDebitCard: 'Direct debit card'
+  }
+};
