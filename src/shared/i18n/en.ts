1222fc751436baa95dfa3495162cda35dd8c68d3
Vittorio Adesso <adessovittorio8@gmail.com>
2023-07-19 20:49:44 +0200

    Modify dictionary

--- src/shared/i18n/en.ts
+++ src/shared/i18n/en.ts
@@ -1,18 +1,13 @@
 export default {
   common: {
     confirm: 'Confirm',
-    confirmPayToUniversity: 'Confirm payment to ADE',
     confirmYourPay: 'Confirm your payment',
     description: 'Description',
     directDebit: 'Direct Debit',
-    directDebitPayments: 'Direct Debit Payments',
     help: 'Help',
-    load: 'Loading...',
     name: 'Name',
-    payConfirm: 'Confirm payment',
     payment: 'Payment',
     titleInfo: 'Card information',
-    university: 'ADE'
   },
   enrolmentTokenization: {
     infoMessage:
